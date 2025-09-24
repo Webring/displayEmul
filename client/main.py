013e5d93b9725c4dca8a1b3c3fd418d4ceb38505
Webring <webringoff@gmail.com>
2025-09-24 13:03:11 +0700

    feat: add passing parameters via ENV and command line parameters

--- client/main.py
+++ client/main.py
@@ -1,11 +1,35 @@
+import argparse
+
 import cv2
 
 from monitor_client import MonitorClient
 
-if __name__ == "__main__":
-    monitor_client = MonitorClient()
-    monitor_client.connect("/tmp/monitor.sock")
+def parse_args():
+    parser = argparse.ArgumentParser(description="Отправка на монитор изображения")
 
-    monitor_client.send_image(cv2.imread("img_2.png"))
+    parser.add_argument(
+        "--image",
+        type=str,
+        required=True,
+        help="Путь к изображению"
+    )
+
+    parser.add_argument(
+        "--sock",
+        type=str,
+        required=True,
+        help="Путь к сокету"
+    )
+
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    monitor_client = MonitorClient()
+    monitor_client.connect(args.sock)
+
+    image = cv2.imread(args.image)
+    monitor_client.send_image(image)
 
     monitor_client.close()
